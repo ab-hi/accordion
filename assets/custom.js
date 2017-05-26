f9e50e67e14e62be36391dc2751141baad87abc4
ab-hi <bansal.abhishek101@yahoo.com>
2017-05-26 20:21:59 +0530

    rectified

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
 
+
+
   $('.accordion a').click(function() {
   	
-    $('.content').slideUp().hide('slow').siblings('a').children('span').text('+');
+    if($(this).is('.active')){
 
-  
-    $(this).siblings('.content').slideDown().siblings('a').children('span').text('-');
+    	
+    $('.content').slideUp().hide('slow').siblings('a').removeClass('active').css('padding-left', '19px').children('span').text('+');
+
+  	}
+
+
+  	else{
+
+  	$('.content').slideUp().hide('slow').siblings('a').removeClass('active').css('padding-left', '19px').children('span').text('+');
+
+    $(this).siblings('.content').slideDown().siblings('a').addClass('active').css('padding-left', '23px').children('span').text('-');
+  }
+
   });
 
  });
